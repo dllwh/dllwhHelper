5cdf0299e4d35b8763fed3eeb09a4c126707b12e
独泪了无痕 <duleilewuhen@sina.com>
2018-10-14 23:00:40 +0800

    浏览器相关

---
+++ dllwh-common/src/main/java/org/dllwh/common/browser/BrowserType.java
@@ -0,0 +1,17 @@
+package org.dllwh.common.browser;
+
+/**
+ * 
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 浏览器类型
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2018年10月14日 下午10:59:24
+ * @版本: V1.0
+ * @since: JDK 1.8
+ */
+public enum BrowserType {
+	IE11, IE10, IE9, IE8, IE7, IE6, Firefox, Safari, Chrome, Opera, Camino, Gecko
+}
