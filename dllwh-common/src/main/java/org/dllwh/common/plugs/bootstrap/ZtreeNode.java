24879daf88868a9e609bc8040c50ea529145f5f3
独泪了无痕 <duleilewuhen@sina.com>
2018-10-14 22:08:33 +0800

    外部插件

---
+++ dllwh-common/src/main/java/org/dllwh/common/plugs/bootstrap/ZtreeNode.java
@@ -0,0 +1,161 @@
+/**
+ * 
+ */
+package org.dllwh.common.plugs.bootstrap;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: jQuery “树插件”
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2018年10月14日 下午10:06:22
+ * @版本: V1.0
+ * @since: JDK 1.8
+ */
+public final class ZtreeNode implements Serializable {
+	private static final long serialVersionUID = 1L;
+	/** 节点的标识属性 */
+	private Integer id;
+	/** 节点parentId属性，命名规则同id */
+	private Integer pId;
+	/** 节点显示的文本 */
+	private String name;
+	/** 节点是否勾选 */
+	private boolean checked;
+	/** 节点的 checkbox / radio 是否禁用;true :节点禁用;false:节点可以使用 */
+	private boolean chkDisabled = false;
+	/** 设置节点是否隐藏 checkbox / radio */
+	private boolean nocheck = false;
+	/** 节点是否展开 */
+	private boolean open = false;
+	/** 节点的图标 */
+	private String icon;
+	/** 节点展开式的图标 */
+	private String iconOpen;
+	/** 节点折叠时的图标 */
+	private String iconClose;
+	/** 判断 treeNode 节点是否被隐藏 ;true 表示被隐藏;false 表示被显示 */
+	private boolean isHidden = false;
+	/** 是否为父节点 */
+	private boolean IsParent = false;
+	/** 得到该节点所有孩子节点，直接下级，若要得到所有下属层级节点，需要自己写递归得到 */
+	private List<ZtreeNode> children;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getpId() {
+		return pId;
+	}
+
+	public void setpId(Integer pId) {
+		this.pId = pId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isChecked() {
+		return checked;
+	}
+
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+
+	public boolean isChkDisabled() {
+		return chkDisabled;
+	}
+
+	public void setChkDisabled(boolean chkDisabled) {
+		this.chkDisabled = chkDisabled;
+	}
+
+	public boolean isNocheck() {
+		return nocheck;
+	}
+
+	public void setNocheck(boolean nocheck) {
+		this.nocheck = nocheck;
+	}
+
+	public boolean isOpen() {
+		return open;
+	}
+
+	public void setOpen(boolean open) {
+		this.open = open;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public String getIconOpen() {
+		return iconOpen;
+	}
+
+	public void setIconOpen(String iconOpen) {
+		this.iconOpen = iconOpen;
+	}
+
+	public String getIconClose() {
+		return iconClose;
+	}
+
+	public void setIconClose(String iconClose) {
+		this.iconClose = iconClose;
+	}
+
+	public boolean getIsHidden() {
+		return isHidden;
+	}
+
+	public void setIsHidden(boolean isHidden) {
+		this.isHidden = isHidden;
+	}
+
+	public boolean getIsParent() {
+		return IsParent;
+	}
+
+	public void setIsParent(boolean isParent) {
+		IsParent = isParent;
+	}
+
+	public List<ZtreeNode> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<ZtreeNode> children) {
+		this.children = children;
+	}
+
+	@Override
+	public String toString() {
+		return "ZtreeNode [id=" + id + ", pId=" + pId + ", name=" + name + ", checked=" + checked
+				+ ", chkDisabled=" + chkDisabled + ", nocheck=" + nocheck + ", open=" + open
+				+ ", icon=" + icon + ", iconOpen=" + iconOpen + ", iconClose=" + iconClose
+				+ ", isHidden=" + isHidden + ", IsParent=" + IsParent + ", children=" + children
+				+ "]";
+	}
+}
---
+++ dllwh-common/src/main/java/org/dllwh/common/plugs/easyui/TreeNode.java
@@ -0,0 +1,96 @@
+package org.dllwh.common.plugs.easyui;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class TreeNode implements Serializable {
+	private static final long serialVersionUID = 1L;
+	/** ----------------------------------------------------- Fields start */
+	/** 绑定节点的标识值 */
+	private Integer id;
+	/** 绑定节点父节点的标识值 */
+	private Integer pid;
+	/** 显示的节点文本 */
+	private String text;
+	/** 节点状态，'open' 或 'closed'。 */
+	private String state = "open";
+	/** 显示的节点图标CSS类ID */
+	private String iconCls;
+	/** 该节点是否被选中 */
+	private boolean checked;
+	/** 绑定该节点的自定义属性参数 */
+	private Object attributes;
+	/** 子节点 */
+	private List<TreeNode> children;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getPid() {
+		return pid;
+	}
+
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getIconCls() {
+		return iconCls;
+	}
+
+	public void setIconCls(String iconCls) {
+		this.iconCls = iconCls;
+	}
+
+	public boolean isChecked() {
+		return checked;
+	}
+
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+
+	public Object getAttributes() {
+		return attributes;
+	}
+
+	public void setAttributes(Object attributes) {
+		this.attributes = attributes;
+	}
+
+	public List<TreeNode> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<TreeNode> children) {
+		this.children = children;
+	}
+
+	@Override
+	public String toString() {
+		return "TreeNode [id=" + id + ", pid=" + pid + ", text=" + text + ", state=" + state
+				+ ", iconCls=" + iconCls + ", checked=" + checked + ", attributes=" + attributes
+				+ ", children=" + children + "]";
+	}
+}
