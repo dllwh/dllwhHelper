2325a11b2aeca62e5ea13354b561e41ce01425de
独泪了无痕 <duleilewuhen@sina.com>
2018-11-11 23:12:08 +0800

    集合工具类(继承于Apache collections)

---
+++ dllwh-utils/src/main/java/org/dllwh/utils/apache/collection/CollectionHelper.java
@@ -0,0 +1,16 @@
+package org.dllwh.utils.apache.collection;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 集合工具类(继承于Apache collections)
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2018年11月11日 下午11:11:20
+ * @版本: V1.0.1
+ * @since: JDK 1.7
+ */
+public class CollectionHelper {
+
+}
