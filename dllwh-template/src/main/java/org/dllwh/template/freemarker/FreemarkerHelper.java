16c20a47cc31a4da875397d0cd8af2414cffe318
独泪了无痕 <duleilewuhen@sina.com>
2018-11-11 22:54:23 +0800

    Freemarker引擎协助类

---
+++ dllwh-template/src/main/java/org/dllwh/template/freemarker/FreemarkerHelper.java
@@ -0,0 +1,17 @@
+package org.dllwh.template.freemarker;
+
+/**
+ * 
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: Freemarker引擎协助类
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2018年10月20日 上午1:50:16
+ * @版本: V1.0.1
+ * @since: JDK 1.8
+ */
+public class FreemarkerHelper {
+
+}
