6e97417792680f2f6a270d57df5d3a314d7c9ef3
独泪了无痕 <duleilewuhen@sina.com>
2018-11-11 22:57:51 +0800

    apache.collection 接口说明文档

---
+++ dllwh-utils/src/main/java/org/dllwh/utils/apache/collection/doc/ListUtilsDoc.java
@@ -0,0 +1,17 @@
+package org.dllwh.utils.apache.collection.doc;
+
+/**
+ * 
+ * @类描述: ListUtils 方法说明文档
+ * @创建者: 独泪了无痕
+ * @创建日期: 2015年8月29日 下午2:22:50
+ * @版本: V1.0
+ * @since: JDK 1.7
+ * @see <a href="commons.apache.org/proper/commons-collections/javadocs/"></a>
+ * @see <a href=
+ *      "commons.apache.org/proper/commons-collections/javadocs/api-4.0/org/apache/commons/collections4/">
+ *      org.apache.commons.collections4</a>
+ */
+interface ListUtilsDoc {
+
+}
