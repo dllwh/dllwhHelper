1fcd20be483ced6e49bf37143150ee6322204d47
独泪了无痕 <duleilewuhen@sina.com>
2019-05-26 18:25:20 +0800

    转码工具类

---
+++ dllwh-common/src/main/java/org/dllwh/common/network/URLEncodingUtil.java
@@ -0,0 +1,23 @@
+package org.dllwh.common.network;
+
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+
+/**
+ * @Description: 转码工具类
+ * @author: 独泪了无痕
+ * @date: 2015年11月1日 下午6:44:19
+ * @version: V1.0
+ * @since: JDK 1.7
+ */
+public class URLEncodingUtil {
+	public static String encoding(final String content, final Charset charset) {
+		String result = "";
+		try {
+			result = URLEncoder.encode(content, charset.name());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+}
