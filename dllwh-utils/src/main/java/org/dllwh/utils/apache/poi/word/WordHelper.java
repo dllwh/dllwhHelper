3d7b3c0f7e062a1ed245839598c8262c3fd40029
独泪了无痕 <duleilewuhen@sina.com>
2019-05-26 18:33:35 +0800

    Apache POI[待实现]

---
+++ dllwh-utils/src/main/java/org/dllwh/utils/apache/poi/word/WordHelper.java
@@ -0,0 +1,16 @@
+package org.dllwh.utils.apache.poi.word;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 提供读写Microsoft Word格式档案的功能
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2019年5月7日 下午11:52:10
+ * @版本: V1.0.1
+ * @since: JDK 1.8
+ */
+public final class WordHelper {
+
+}
