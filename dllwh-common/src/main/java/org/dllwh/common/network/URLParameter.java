2a92ac61430e3f47b94ab275325c593b992ead15
独泪了无痕 <duleilewuhen@sina.com>
2019-05-26 18:25:39 +0800

    URL 操作工具类

---
+++ dllwh-common/src/main/java/org/dllwh/common/network/URLParameter.java
@@ -0,0 +1,50 @@
+package org.dllwh.common.network;
+
+import org.dllwh.common.constanst.ConstantHelper;
+
+/**
+ * @描述 : 键值对参数
+ * @author: 独泪了无痕
+ * @date: 2015-10-15 下午08:07:03
+ * @version: V1.0
+ */
+public class URLParameter extends NameValue {
+	private static final long serialVersionUID = 1L;
+
+	public URLParameter(String name, String value) {
+		super(name, value);
+	}
+
+	public String encoding() {
+		return String.format("%s=%s", URLEncodingUtil.encoding(getName(), ConstantHelper.UTF_8),
+				URLEncodingUtil.encoding(getValue(), ConstantHelper.UTF_8));
+	}
+
+	@Override
+	public String toString() {
+		return String.format("【URLParameter name = %s,value=%S】", getName(), getValue());
+	}
+
+	/**
+	 * 连接字符串.
+	 * 
+	 * @param connector
+	 * @param arr
+	 * @return
+	 */
+	public static String join(String connector, String[] arr) {
+
+		StringBuffer sBuffer = new StringBuffer();
+
+		for (Object object : arr) {
+			sBuffer.append(object).append(connector);
+		}
+
+		if (sBuffer.length() > 0) {
+			sBuffer.delete(sBuffer.length() - connector.length(), sBuffer.length());
+		}
+
+		return sBuffer.toString();
+	}
+
+}
