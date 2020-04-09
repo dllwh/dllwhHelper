f4e18c3cdff2fd4a9bd5ca016badc95373ecf919
独泪了无痕 <duleilewuhen@sina.com>
2020-04-10 00:32:27 +0800

    MyBatis Generator 的使用及配置

--- dllwh-template/src/main/java/org/dllwh/template/database/mybatis/GeneratorSqlmap.java
+++ dllwh-template/src/main/java/org/dllwh/template/database/mybatis/GeneratorSqlmap.java
@@ -11,17 +11,24 @@ import org.mybatis.generator.internal.DefaultShellCallback;
 
 public class GeneratorSqlmap {
 	public void generator() throws Exception {
-
+		// MyBatis-Generator 执行过程中的警告信息
 		List<String> warnings = new ArrayList<String>();
+		// 当生成的代码重复时，覆盖原代码
 		boolean overwrite = true;
 		// 指向逆向工程配置文件
 		String genCfg = "/generatorConfig.xml";
+		// 读取 MyBatis-Generator 配置文件
 		File configFile = new File(GeneratorSqlmap.class.getResource(genCfg).getFile());
 		ConfigurationParser cp = new ConfigurationParser(warnings);
 		Configuration config = cp.parseConfiguration(configFile);
+
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		// 创建 MyBatis-Generator
 		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		// 执行生成代码
 		myBatisGenerator.generate(null);
+		// 输出警告信息
+		warnings.forEach(warning -> System.out.println(warning));
 
 	}
 
