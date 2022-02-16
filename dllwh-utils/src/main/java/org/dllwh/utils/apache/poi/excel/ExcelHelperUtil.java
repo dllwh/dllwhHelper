ec306ec23b20cbeeb74d7a22953d20f26c8cee5c
独泪了无痕 <duleilewuhen@sina.com>
2022-02-16 23:56:39 +0800

    引入 org.apache.poi 依赖

--- dllwh-utils/src/main/java/org/dllwh/utils/apache/poi/excel/ExcelHelperUtil.java
+++ dllwh-utils/src/main/java/org/dllwh/utils/apache/poi/excel/ExcelHelperUtil.java
@@ -1,8 +1,11 @@
 package org.dllwh.utils.apache.poi.excel;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+
 /**
  * 把今天最好的表现当作明天最新的起点．．～
- *
+ * <p>
  * Today the best performance as tomorrow newest starter!
  *
  * @类描述: POI提供API给Java程序对Microsoft Office格式档案读和写的功能
@@ -12,5 +15,24 @@ package org.dllwh.utils.apache.poi.excel;
  * @since: JDK 1.8
  */
 public final class ExcelHelperUtil {
-
+    /**
+     * 创建文件输入流
+     */
+    private BufferedReader reader = null;
+    /**
+     * 文件类型
+     */
+    private String fileType;
+    /**
+     * 文件二进制输入流
+     */
+    private InputStream inputStream;
+    /**
+     * 当前的Sheet
+     */
+    private int currentSheet;
+    /**
+     * 当前位置
+     */
+    private int currentPosition;
 }
