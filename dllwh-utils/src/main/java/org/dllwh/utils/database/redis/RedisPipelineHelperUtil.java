e16b91d1e174a7bdb5b620a1cdae35428bafa7a6
独泪了无痕 <duleilewuhen@sina.com>
2022-12-01 23:03:26 +0800

    Jedis 单节点 操作管道模式

--- dllwh-utils/src/main/java/org/dllwh/utils/database/redis/RedisPipelineHelperUtil.java
+++ dllwh-utils/src/main/java/org/dllwh/utils/database/redis/RedisPipelineHelperUtil.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.*;
 import redis.clients.jedis.*;
 
+import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -11,14 +12,14 @@ import java.util.concurrent.locks.ReentrantLock;
  * <p>
  * Today the best performance as tomorrow the newest starter!
  *
- * @类描述: Jedis 单节点使用
+ * @类描述: Redis 管道模式
  * @author: <a href="mailto:duleilewuhen@sina.com">独泪了无痕</a>
- * @创建时间: 2022-11-16 01:15
+ * @创建时间: 2022-12-01 09:03
  * @版本: V 1.0.1
  * @since: JDK 1.8
  */
-public class JedisPoolHelperUtil {
-    private static final Logger logger = LoggerFactory.getLogger(JedisPoolHelperUtil.class);
+public class RedisPipelineHelperUtil {
+    private static final Logger logger = LoggerFactory.getLogger(RedisPipelineHelperUtil.class);
     /**
      * jedis连接池。
      * <p>
@@ -26,6 +27,7 @@ public class JedisPoolHelperUtil {
      */
     private static volatile JedisPool jedisPool;
     private final static ReentrantLock LOCK_POOL = new ReentrantLock();
+
     /**
      * Redis节点所在的机器的IP
      */
@@ -41,26 +43,50 @@ public class JedisPoolHelperUtil {
     /**
      * Redis池配置
      */
-    private final static JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+    private static JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
 
-    private JedisPoolHelperUtil() {
-    }
 
-    public JedisPoolHelperUtil(String host, int port) {
+    public RedisPipelineHelperUtil(String host, int port) {
         this.host = host;
         this.port = port;
         initPool();
     }
 
-    public JedisPoolHelperUtil(String host, int port, String password) {
+    public RedisPipelineHelperUtil(String host, int port, String password) {
         this.host = host;
         this.port = port;
         this.password = password;
         initPool();
     }
 
-    static {
-        getJedisPoolConfig();
+    /**
+     * 初始化单节点池，只会调用一次
+     */
+    private synchronized void initPool() {
+
+        logger.debug("host:[{}], port:[{}].", host, port);
+        // 断言 ，当前锁是否已经锁住，如果锁住了，就啥也不干，没锁的话就执行下面步骤
+        assert !LOCK_POOL.isHeldByCurrentThread();
+        LOCK_POOL.lock();
+        try {
+            if (jedisPool == null) {
+                synchronized (JedisPoolHelper.class) {
+                    if (jedisPool == null) {
+                        // 设置连接到主节点
+                        if (StringUtils.isNotBlank(password)) {
+                            jedisPool = new JedisPool(jedisPoolConfig, host, port, Protocol.DEFAULT_TIMEOUT, password);
+                        } else {
+                            jedisPool = new JedisPool(jedisPoolConfig, host, port);
+                        }
+                    }
+                }
+            }
+        } catch (Exception exp) {
+            // 创建Redis Pool失败
+            exp.printStackTrace();
+        } finally {
+            LOCK_POOL.unlock();
+        }
     }
 
     /**
@@ -79,34 +105,6 @@ public class JedisPoolHelperUtil {
         jedisPoolConfig.setTestOnReturn(true);
         // 连接耗尽的时候，是否阻塞，false会抛出异常，true阻塞直到超时。默认为true。
         jedisPoolConfig.setBlockWhenExhausted(true);
-    }
-
-    /**
-     * 初始化单节点池，只会调用一次
-     */
-    private synchronized void initPool() {
-        logger.debug("host:[{}], port:[{}].", host, port);
-        // 断言 ，当前锁是否已经锁住，如果锁住了，就啥也不干，没锁的话就执行下面步骤
-        assert !LOCK_POOL.isHeldByCurrentThread();
-        LOCK_POOL.lock();
-        try {
-            if (jedisPool == null) {
-                synchronized (JedisPoolHelper.class) {
-                    if (jedisPool == null) {
-                        if (StringUtils.isNotBlank(password)) {
-                            jedisPool = new JedisPool(jedisPoolConfig, host, port, Protocol.DEFAULT_TIMEOUT, password);
-                        } else {
-                            jedisPool = new JedisPool(jedisPoolConfig, host, port);
-                        }
-                    }
-                }
-            }
-        } catch (Exception exp) {
-            // 创建Redis Pool失败
-            exp.printStackTrace();
-        } finally {
-            LOCK_POOL.unlock();
-        }
     }
 
     /**
@@ -129,12 +127,31 @@ public class JedisPoolHelperUtil {
         }
     }
 
-    public static void main(String[] args) {
-        JedisPoolHelperUtil jedisPoolUtils = new JedisPoolHelperUtil("127.0.0.1", 6379);
+    public static void main(String[] args) throws Exception {
+        RedisPipelineHelperUtil jedisPoolUtils = new RedisPipelineHelperUtil("127.0.0.1", 6379);
+        //从redis连接池里拿出一个连接执行命令
         Jedis redisClient = jedisPoolUtils.getRedisClient();
-        redisClient.set("msg", "Hello World!");
-        System.out.println(redisClient.get("msg"));
-        System.out.println(redisClient.dbSize());
-        jedisPoolUtils.release(redisClient);
+        try {
+
+            redisClient = jedisPool.getResource();
+
+            //管道示例
+            //管道的命令执行方式：cat redis.txt | redis-cli -h 127.0.0.1 -a password - p 6379 --pipe
+            Pipeline pl = redisClient.pipelined();
+            for (int i = 0; i < 10; i++) {
+                pl.incr("pipelineKey");
+                pl.set("sch" + i, i + "");
+            }
+            List<Object> results = pl.syncAndReturnAll();
+            //结果显示：[1, OK, 2, OK, 3, OK, 4, OK, 5, OK, 6, OK, 7, OK, 8, OK, 9, OK, 10, OK]
+            System.out.println(results);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //注意这里不是关闭连接，在JedisPool模式下，Jedis对象会被归还给资源池。
+            jedisPoolUtils.release(redisClient);
+        }
     }
 }
